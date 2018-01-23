ace13496a025fb67a06d8001f61a21b8ce793259
Thomas Chung <thomas@nomology.id.au>
2018-01-23 17:06:29 +1100

    Make dump-der.py work with python3
    (cherry picked from commit 366d476ee7938837cd8080824f6d853348e57193)

--- src/cert/dump-der.py
+++ src/cert/dump-der.py
@@ -9,9 +9,7 @@ def pk_to_c_array(name, pk_der):
     pk_native = pk['private_key'].native
 
     # translate to hex string
-    pk_hex = hex(pk_native)
-    # remove leading 0x and trailing L
-    pk_hex = pk_hex[2:-1]
+    pk_hex = format(pk_native, 'x')
     # pad with zeros to 32 bytes
     pk_hex = ("0" * (64 - len(pk_hex))) + pk_hex
 
@@ -23,15 +21,18 @@ def pk_to_c_array(name, pk_der):
 
 def cert_to_c_array(name, der):
     defname = name.upper() + "_LEN"
-    hex_str = der.encode('hex')
+    if hasattr(der, 'hex'):
+        hex_str = der.hex()
+    else:
+        hex_str = der.encode('hex')
     hex_bytes = ["0x" + hex_str[i:i + 2] for i in range(0, len(hex_str), 2)]
 
     define = "#define " + defname + " " + str(len(der))
     array = "const uint8_t " + name + "[" + defname + "] = {" + ", ".join(hex_bytes) + "};"
     return define + "\n" + array
 
-with open("attestation.der", "r") as f:
-    print cert_to_c_array("attestation_der", f.read())
+with open("attestation.der", "rb") as f:
+    print(cert_to_c_array("attestation_der", f.read()))
 
 with open("attestation_key.der", "r") as f:
     sys.stdout.write(pk_to_c_array("attestation_key", f.read()))
