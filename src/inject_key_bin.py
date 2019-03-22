71621e32415bfe7d2b4409947642945d460d1b36
Eliot Blennerhassett <eliot@blennerhassett.gen.nz>
2019-03-22 14:18:46 +1300

    typo in param help string update
    Signed-off-by: Eliot Blennerhassett <eliot@blennerhassett.gen.nz>

--- src/inject_key_bin.py
+++ src/inject_key_bin.py
@@ -25,7 +25,7 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--bin", default="build/u2f.bin",
-                    help='.bin file to inject keys into. Or "stdin"'
+                    help='.bin file to inject keys into. Or "stdin"')
 parser.add_argument("--key", help="EC private key in DER format")
 parser.add_argument("--ctr", default=0, type=int, help="Value of auth counter")
 parser.add_argument("--offset", default=0xB400, type=int, help="Offset within file to patch")
