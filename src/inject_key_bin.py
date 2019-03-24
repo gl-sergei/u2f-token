895f7b0d8486398126302cdd5f896b4ee9045313
Eliot Blennerhassett <eliot@blennerhassett.gen.nz>
2019-03-25 11:45:11 +1300

    Clarify offsets

--- src/inject_key_bin.py
+++ src/inject_key_bin.py
@@ -10,7 +10,7 @@
 # > openssl ecparam -name prime256v1 -genkey -noout -outform der > key.der
 #
 # Inject generated key into u2f.bin and set auth counter to 100:
-# > python3 inject_key_bin.py --key key.der --ctr 100
+# > python3 inject_key_bin.py --key key.der --ctr 100 --bin build/u2f.bin
 #
 # key will not be modified if --key parameter is not present
 # counter will not be modified if --ctr parameter is not present
@@ -23,25 +23,29 @@ import sys
 import struct
 import os
 
+# TODO: detect correct offset from .bin file.
+stm32f103_offset = 0xB800  # See stm32f103.ld
+#efm32hg_offset = ????
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--bin", default="build/u2f.bin",
                     help='.bin file to inject keys into. Or "stdin"')
 parser.add_argument("--key", help="EC private key in DER format")
 parser.add_argument("--ctr", default=0, type=lambda x: int(x,0), help="Value of auth counter")
-parser.add_argument("--offset", default=0xB800, type=lambda x: int(x,0), help="Offset within file to patch")
+parser.add_argument("--offset", default=stm32f103_offset, type=lambda x: int(x,0), help="Offset within file to patch")
 args = parser.parse_args()
 
 fname, fext = os.path.splitext(args.bin)
 assert fext == ".bin"
 
 print("Target binary file:", args.bin)
-print("Key injection offset 0x%0X" % args.offset)
 
 # load and parse private key
 if not args.key:
     print("Key not modified")
 else:
-    print("Injecting key from", args.key)
+    key_offset = args.offset
+    print("Injecting key from %s at 0x%0X", (args.key, key_offset)
     if args.key == "stdin":
         stdin = sys.stdin.buffer if hasattr(sys.stdin, "buffer") else sys.stdin
         der = stdin.read()
@@ -58,16 +62,17 @@ else:
     assert len(key_blob) == 1024
 
     with open(args.bin, 'r+b') as f:
-        f.seek(args.offset)
+        f.seek(key_offset)
         f.write(key_blob)
 
 if not args.ctr:
     print("Counter not modified")
 else:
-    print("Injecting counter", args.ctr)
+    ctr_offset = args.offset + 0x400
+    print("Injecting counter %d at 0x%0X", (args.ctr, ctr_offset)
     # fill authentication counter
     ctr_blob = struct.pack("<I", args.ctr) * 256
 
     with open(args.bin, 'r+b') as f:
-        f.seek(args.offset + 1024)
+        f.seek(ctr_offset)
         f.write(ctr_blob)
